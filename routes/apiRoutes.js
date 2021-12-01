adf85282ed15762130953ddd18ff3f618e571114
Jeffrey <hernew@gmail.com>
2021-12-01 08:18:26 -0800

    finished sotrage js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,7 +11,19 @@ router.get('/notes', (req,res) => {
     }).catch((err) =>res.status(500).json(err))
 })
 
-// post 
-// delete
+router.post("/notes", (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch(err => res.status(500).json(err));
+  });
+  
+  // DELETE "/api/notes" deletes the note with an id equal to req.params.id
+  router.delete("/notes/:id", function(req, res) {
+    store
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch(err => res.status(500).json(err));
+  });
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,16 @@
 // one route ('*')
 // another route ('/notes')
+const router = require('express').Router();
+const path = require('path');
+
+// "/notes" responds with the notes.html file
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  
+  // All other routes respond with the index.html file
+  router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+  module.exports = router;
